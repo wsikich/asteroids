78aa356a392202ecaf634e3ba5928f81bac1f910
wsikich <wsikich324@gmail.com>
2025-07-30 11:16:25 -0500

    add constants.py and add fps to main.py

--- main.py
+++ main.py
@@ -8,12 +8,17 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
     
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
