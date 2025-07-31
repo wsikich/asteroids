f370a4d36cc2c5070cb41aec6ce1d0381c3050cf
wsikich <wsikich324@gmail.com>
2025-07-31 10:43:01 -0500

    add player.py and circleshape.py, implement rotation of player ship

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pygame
 
 from constants import *
+from player import *
 
 
 def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    avatar = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     clock = pygame.time.Clock()
 
@@ -17,8 +20,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        avatar.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+        avatar.update(dt)
     
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
