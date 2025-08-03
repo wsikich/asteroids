babd5ef37615d54aa6720959b89c8914bd78ffdd
wsikich <wsikich324@gmail.com>
2025-08-03 09:20:48 -0500

    add asteroids

---
+++ asteroid.py
@@ -0,0 +1,17 @@
+import pygame
+
+from circleshape import *
+
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.x = x
+        self.y = y
+        self.radius = radius
+    
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", (self.position), self.radius, 2)
+
+    def update(self, dt):
+        self.position += (self.velocity * dt)
